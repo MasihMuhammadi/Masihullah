4807fc5d303962e08506f2ffd51836e0c1088809
MasihMuhammadi <masihmuhammadi202@gamil.com>
2023-05-23 03:01:58 -0700

    fixed bugs

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './app.css'
+import './App.css'
 
 import bootstrap from '../node_modules/bootstrap/dist/css/bootstrap.css'
 import Navbar from './assets/Navbar.jsx'
