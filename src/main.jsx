75594b613cf4a10bb40ee6d4e8a1243a069d6e2c
MasihMuhammadi <masihmuhammadi202@gamil.com>
2023-05-23 02:47:19 -0700

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import './app.css'
 
 import bootstrap from '../node_modules/bootstrap/dist/css/bootstrap.css'
 import Navbar from './assets/Navbar.jsx'
